368f5f0a225aad5c87a8a1470677544f581c8044
Christian Leonardo <pleasefloyd73@gmail.com>
2024-07-16 16:27:09 -0300

    chore: update deps

--- .eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-const config = {
-  ignorePatterns: ["apps/**", "packages/**", "config/**"],
-  extends: ["@soli/eslint-config/base"]
-}
-
-module.exports = config
