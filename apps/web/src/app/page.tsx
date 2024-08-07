4106818e17c02baaa64cdd94b24eb5958231557e
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-07 13:32:04 -0300

    chore: reset template

--- apps/web/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeSwitcher } from "@/components/theme-switcher"
-
-export default function Home() {
-  return (
-    <div>
-      <ThemeSwitcher />
-    </div>
-  )
-}
--- packages/ui/.storybook/theme.ts
+++
@@ -1,7 +0,0 @@
-import { create } from "@storybook/theming"
-
-export default create({
-  base: "dark",
-  fontBase: "var(--font-geist-sans)",
-  fontCode: "var(--font-geist-mono)"
-})
