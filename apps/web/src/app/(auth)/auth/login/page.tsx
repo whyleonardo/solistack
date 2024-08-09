b34d67d547887fc09055105050e78af775070e14
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-09 13:43:57 -0300

    chore: remove

--- apps/web/src/app/(auth)/auth/login/page.tsx
+++ apps/web/src/app/(auth)/auth/login/page.tsx
@@ -1,9 +1,7 @@
-import { env } from "@soli/env/web"
-
 const LoginPage = async () => {
   return (
     <div className="container flex h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold tracking-tight">{env.AUTH_SECRET}</h1>
+      <h1 className="text-3xl font-bold tracking-tight">Login Page</h1>
     </div>
   )
 }
