56343540f5bd23314a69107c1c20008c99d93e8c
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-09 17:00:00 -0300

    chore: exports compiled auth files

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,11 +1,12 @@
-import NextAuth from "next-auth"
+import NextAuth, { type NextAuthResult } from "next-auth"
 
 import { authConfig } from "./auth.config"
 
-export const {
-  auth,
-  signIn,
-  signOut,
-  unstable_update: update,
-  handlers,
-} = NextAuth(authConfig)
+export const nextAuth = NextAuth(authConfig)
+
+export const signIn: NextAuthResult["signIn"] = nextAuth.signIn
+export const auth: NextAuthResult["auth"] = nextAuth.auth
+export const signOut: NextAuthResult["signOut"] = nextAuth.signOut
+export const update: NextAuthResult["unstable_update"] =
+  nextAuth.unstable_update
+export const handlers: NextAuthResult["handlers"] = nextAuth.handlers
