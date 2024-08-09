1a06146ca2eda5bd12b6a2ded22fd32c593eb422
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-09 17:00:26 -0300

    chore: setup basic login page

--- apps/web/src/app/(auth)/auth/login/page.tsx
+++ apps/web/src/app/(auth)/auth/login/page.tsx
@@ -1,7 +1,26 @@
+import { Suspense } from "react"
+
+import { auth, signIn } from "@soli/auth"
+
 const LoginPage = async () => {
+  const session = await auth()
+
   return (
-    <div className="container flex h-screen items-center justify-center">
+    <div className="container flex h-screen flex-col items-center justify-center">
       <h1 className="text-3xl font-bold tracking-tight">Login Page</h1>
+
+      <form
+        action={async () => {
+          "use server"
+          await signIn("github")
+        }}
+      >
+        <button type="submit">Sign in with GitHub</button>
+      </form>
+
+      <Suspense fallback={<div>Loading User...</div>}>
+        {session?.user?.name}
+      </Suspense>
     </div>
   )
 }
