1799ceade3a75143829bd91a9ee40a01fa5e359d
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-10 15:58:30 -0300

    setup auth package

---
+++ packages/auth/auth.ts
@@ -0,0 +1,20 @@
+import { db } from '@solistack/db';
+import { env } from '@solistack/env/web/server';
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+  }),
+  emailAndPassword: {
+    enabled: false,
+  },
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+  },
+  secret: env.BETTER_AUTH_SECRET,
+});
---
+++ packages/auth/client.ts
@@ -0,0 +1,6 @@
+import { env } from '@solistack/env/web/client';
+import { createAuthClient } from 'better-auth/react';
+
+export const { signIn, signUp, signOut } = createAuthClient({
+  baseURL: env.NEXT_PUBLIC_APP_BASE_URL,
+});
