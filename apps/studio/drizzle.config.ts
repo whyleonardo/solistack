613d27f75f712f806abe1abd3089cc05e161d0f3
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-10 16:10:30 -0300

    add drizzle studio app

---
+++ apps/studio/drizzle.config.ts
@@ -0,0 +1,11 @@
+import { env } from '@solistack/env/web/db';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: '.next',
+  dialect: 'postgresql',
+  schema: ['../../packages/db/schemas'],
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+});
