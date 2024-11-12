cc9eb14a3e1f1ed41e0bf68ce11f1550ae49e19e
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-12 14:57:01 -0300

    move stripe export to end of file

--- packages/payments/index.ts
+++ packages/payments/index.ts
@@ -3,8 +3,8 @@ import { env } from "@solistack/env/web/server"
 import "server-only"
 import Stripe from "stripe"
 
-export type { Stripe } from "stripe"
-
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
   apiVersion: "2024-10-28.acacia",
 })
+
+export type { Stripe } from "stripe"
