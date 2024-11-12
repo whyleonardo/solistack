af86484d3167b2ded017e49aa73832486e77ec14
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-11 21:13:49 -0300

    update envs

--- packages/env/src/web/server.ts
+++ packages/env/src/web/server.ts
@@ -12,6 +12,9 @@ export const env = createEnv({
     GITHUB_CLIENT_ID: z.string(),
     GITHUB_CLIENT_SECRET: z.string(),
     FLAGS_SECRET: z.string().min(1),
+    ALLOWED_ORIGINS: z.string().refine((val) => val.split(",").length > 0),
+    APP_BASE_URL: z.string().url(),
+    API_BASE_URL: z.string().url(),
   },
   experimental__runtimeEnv: {},
 })
