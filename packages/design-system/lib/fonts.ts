787b42c835de2f9c139472ef6fcd82e3c7e5004b
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-12 11:22:10 -0300

    fix fallback font error

--- packages/design-system/lib/fonts.ts
+++ packages/design-system/lib/fonts.ts
@@ -5,12 +5,16 @@ import { cn } from "@solistack/design-system/lib/utils"
 const sans = Geist({
   subsets: ["latin"],
   variable: "--font-geist-sans",
-  fallback: ["Inter", "sans-serif"],
+  fallback: ["sans-serif"],
+  display: "swap",
+  adjustFontFallback: false,
 })
 const mono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-geist-mono",
-  fallback: ["Inter", "monospace"],
+  fallback: ["monospace"],
+  display: "swap",
+  adjustFontFallback: false,
 })
 
 export const fonts = cn(sans.variable, mono.variable)
