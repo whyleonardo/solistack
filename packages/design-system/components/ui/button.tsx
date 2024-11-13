def9244fd431515aaeb799cdf96b5ad3e09926d7
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-13 12:52:47 -0300

    adjust border on outline variant

--- packages/design-system/components/ui/button.tsx
+++ packages/design-system/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = tv({
       destructive:
         "bg-destructive text-destructive-foreground hover:bg-destructive/90 shadow-sm",
       outline:
-        "border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm",
+        "border-input bg-background hover:bg-accent/50 hover:text-accent-foreground border shadow-sm",
       secondary:
         "bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm",
       ghost: "hover:bg-accent hover:text-accent-foreground",
