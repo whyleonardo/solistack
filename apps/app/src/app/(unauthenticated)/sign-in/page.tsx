a4845116d1440cd9baa3cd1e2f3329001a5f85da
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-13 12:55:38 -0300

    add metadata and adjust typography

--- apps/app/src/app/(unauthenticated)/sign-in/page.tsx
+++ apps/app/src/app/(unauthenticated)/sign-in/page.tsx
@@ -1,6 +1,16 @@
-import { SignInCard } from "@/features/auth/components/sign-in-card"
+import type { Metadata } from "next/types"
+
+import { SignIn } from "@/features/auth/components/sign-in"
 import { getSession } from "@/features/auth/utils/get-session"
 import { protectRoute } from "@/features/auth/utils/protect-route"
+
+import { Typography } from "@solistack/design-system/components/ui/typography"
+import { createMetadata } from "@solistack/seo/metadata"
+
+const title = "Welcome!"
+const description = "Use your OAuth account to login"
+
+export const metadata: Metadata = createMetadata({ title, description })
 
 const SignInPage = async () => {
   const session = await getSession()
@@ -10,9 +20,18 @@ const SignInPage = async () => {
   }
 
   return (
-    <>
-      <SignInCard />
-    </>
+    <div className="flex flex-col space-y-4 text-center">
+      <div>
+        <Typography as="h1" variant="h2">
+          {title}
+        </Typography>
+        <Typography variant="muted" as="p" className="">
+          {description}
+        </Typography>
+      </div>
+
+      <SignIn />
+    </div>
   )
 }
 
