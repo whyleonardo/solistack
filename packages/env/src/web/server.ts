674c4286c3d6eb6b3fd88b17b0b495bf52de53c2
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-15 00:59:58 -0300

    remove unused env

--- packages/env/src/web/server.ts
+++ packages/env/src/web/server.ts
@@ -12,7 +12,6 @@ export const env = createEnv({
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
     FLAGS_SECRET: z.string().min(1),
-    ALLOWED_ORIGINS: z.string().refine((val) => val.split(",").length > 0),
     STRIPE_SECRET_KEY: z.string().min(1),
     STRIPE_WEBHOOK_SECRET: z.string().min(1).startsWith("whsec_"),
     BETTERSTACK_URL: z.string().url(),
