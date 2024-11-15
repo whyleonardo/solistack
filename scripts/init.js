f5a382a3da8c72b05691f4edb80c7b32b0a5e204
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-15 00:50:02 -0300

    add step to copy env on studio app

--- scripts/init.js
+++ scripts/init.js
@@ -4,8 +4,9 @@ await $`echo "Installing dependencies...`
 await $`bun install`
 
 await $`echo Copying .env.example files to .env files...`
-await $`cp apps/api/.env.example apps/api/.env.local`
-await $`cp apps/app/.env.example apps/app/.env.local`
+await $`cp apps/api/.env.example apps/api/.env`
+await $`cp apps/studio/.env.example apps/studio/.env`
+await $`cp apps/app/.env.example apps/app/.env`
 await $`cp packages/db/.env.example packages/db/.env`
 
 await $`echo Setup complete! Deleting scripts folder...`
