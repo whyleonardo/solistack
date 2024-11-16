29fa1f3c47b08af85c8e3cecdc7dc6b5bbbf8d3d
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-16 12:29:51 -0300

    add validation prefix on envs

--- packages/env/src/web/client.ts
+++ packages/env/src/web/client.ts
@@ -7,9 +7,9 @@ import { sharedEnv } from "../shared"
 export const env = createEnv({
   extends: [sharedEnv],
   client: {
-    NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1),
+    NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1).startsWith("phc_"),
     NEXT_PUBLIC_POSTHOG_HOST: z.string().min(1),
-    NEXT_PUBLIC_GA_MEASUREMENT_ID: z.string().min(1),
+    NEXT_PUBLIC_GA_MEASUREMENT_ID: z.string().min(1).startsWith("G-"),
     NEXT_PUBLIC_APP_BASE_URL: z.string().url(),
     NEXT_PUBLIC_API_BASE_URL: z.string().url(),
     NEXT_PUBLIC_SENTRY_DSN: z.string().min(1),
--- packages/env/src/web/server.ts
+++ packages/env/src/web/server.ts
@@ -12,7 +12,7 @@ export const env = createEnv({
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
     FLAGS_SECRET: z.string().min(1),
-    STRIPE_SECRET_KEY: z.string().min(1),
+    STRIPE_SECRET_KEY: z.string().min(1).startsWith("sk_"),
     STRIPE_WEBHOOK_SECRET: z.string().min(1).startsWith("whsec_"),
     BETTERSTACK_URL: z.string().url(),
     BETTERSTACK_API_KEY: z.string().min(1),
