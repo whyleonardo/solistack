0c03826db06f9288eef139e61c219aed2be2b9df
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-15 22:33:35 -0300

    transpile env package on main app next config

--- apps/app/next.config.ts
+++ apps/app/next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next"
 import { env } from "@solistack/env/web/server"
 import { config, withSentry } from "@solistack/next-config"
 
-let nextConfig: NextConfig = { ...config }
+let nextConfig: NextConfig = {
+  transpilePackages: ["@solistack/env"],
+  ...config,
+}
 
 if (env.VERCEL) {
   nextConfig = withSentry(nextConfig)
